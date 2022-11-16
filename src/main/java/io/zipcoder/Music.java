9f6264b5cf296d6362405958cc22822ccec43e50
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-11-15 21:40:05 -0500

    added 2 methods

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,34 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+     int upcount = 0;
+     int downcount = 0;
+     int idx = startIndex;
+     while(upcount<this.playList.length) {
+         if (this.playList[idx].equals(selection))
+             break;
+             upcount++;
+             idx++;
+
+         if(idx>playList.length-1)
+             idx = 0;
+         
+     }
+
+     while(downcount<this.playList.length){
+         if(this.playList[idx].equals(selection))
+             break;
+             downcount++;
+         idx--;
+         if(idx < 0)
+             idx = playList.length-1;
+
+     }
+     if(upcount <= downcount){
+         return upcount;
+     }
+
+        return downcount;
     }
 }
